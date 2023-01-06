a058fd0749eaa98013bd0356b0de34eae11d255f
Tulio Amancio <tulioromeror@gmail.comm>
2023-01-05 22:28:39 -0300

    implementing raffle delete method

--- app/routers/raffle.py
+++ app/routers/raffle.py
@@ -95,3 +95,15 @@ def update_raffle(id: str, payload: schemas.RaffleUpdateSchema, user_id: str = D
                             detail=f'No raffle with this id: {id} found')
         
     return raffleEntity(updated_raffle)
+
+@router.delete('/{id}')
+def delete_raffle(id: str, user_id: str = Depends(require_user)):
+    if not ObjectId.is_valid(id):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Invalid id: {id}")
+    raffle = Raffle.find_one_and_delete({'_id': ObjectId(id)})
+    if not raffle:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'No raffle with this id: {id} found')
+        
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
