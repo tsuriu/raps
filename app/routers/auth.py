e41f0e5408c5ad8dc7da40a0a4ceae1a480d4f9f
Tulio Amancio <tulioromeror@gmail.comm>
2023-01-05 22:40:05 -0300

    cors test

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -93,7 +93,7 @@ def login(payload: schemas.LoginUserSchema, response: Response, Authorize: AuthJ
                         ACCESS_TOKEN_EXPIRES_IN * 60, '/', None, False, False, 'lax')
 
     # Send both access
-    return {'status': 'success', 'access_token': access_token}
+    return {'status': 'success', 'access_token': access_token, 'refresh_token': refresh_token}
 
 
 @router.get('/refresh')
