ae75a977993863dd57b951e7fbaccb8465668fcb
Tulio Amancio <tulioromeror@gmail.comm>
2023-01-21 01:51:10 -0300

    Starting user role check feature

--- app/oauth2.py
+++ app/oauth2.py
@@ -8,6 +8,7 @@ from bson.objectid import ObjectId
 from app.serializers.userSerializers import userEntity
 
 from .database import User
+from .schemas import UserBaseSchema
 from .config import settings
 
 
@@ -74,3 +75,12 @@ def require_user(Authorize: AuthJWT = Depends()):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail='Token is invalid or has expired')
     return user_id
+
+
+class RoleChecker:
+    def __init__(self, allowed_roles: List):
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, user: UserBaseSchema = Depends(require_user)):
+        if user.role not in self.allowed_roles:
+            raise HTTPException(status_code=403, detail="Operation not permitted")
