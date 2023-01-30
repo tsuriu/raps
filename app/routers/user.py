28b4a182699d19ffb3878214590ebc3779615166
Tulio Amancio <tulioromeror@gmail.comm>
2023-01-30 19:02:01 -0300

    Update user endpoint fixing role restritions

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from fastapi import APIRouter, Depends, HTTPException, status, Response
 from pymongo.collection import ReturnDocument
 
@@ -7,7 +9,9 @@ from app.serializers.userSerializers import userResponseEntity, userEntity, user
 from app.database import User
 from .. import schemas, oauth2
 
-from app.oauth2 import require_user
+from app.oauth2 import require_user, RoleChecker
+
+allow_raffle_creation = RoleChecker(["admin","user"])
 
 
 router = APIRouter()
@@ -46,14 +50,25 @@ def get_users(limit: int = 10, page: int = 1, user_id: str = Depends(require_use
     users = userListEntity(User.aggregate(pipeline))
     return {'status': 'success', 'results': len(users), 'users':users}
 
-
-@router.put("/{id}")
+@router.put(
+    "/{id}",
+    dependencies=[Depends(allow_raffle_creation)]
+)
 def update_me(id: str, payload: schemas.UserUpdateSchema, user_id: str = Depends(require_user)):
     if not ObjectId.is_valid(id):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"Invalid id: {id}")
-    updated_user = User.find_one_and_update(
-        {'_id': ObjectId(id)}, {'$set': payload.dict(exclude_none=True)}, return_document=ReturnDocument.AFTER)
+        
+    user = userEntity(User.find_one({'_id': ObjectId(str(user_id))}))
+    
+    if user_id != user["_id"] or user["role"] != "admin":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Operation not permitted")
+        
+    payload.updated_at = datetime.utcnow()
+        
+    updated_user = User.find_one_and_update({'_id': ObjectId(id)}, {'$set': payload.dict(exclude_none=True)}, return_document=ReturnDocument.AFTER)
+    
     if not updated_user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'No user with this id: {id} found')
